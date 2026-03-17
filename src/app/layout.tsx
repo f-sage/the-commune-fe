8d87ba7ebc665341df7ab736b68e1a1a71784f59
f-sage <junia.en@gmail.com>
2026-03-17 17:58:45 +0200

    Add the navbar for site pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./layout.css";
+import { Navbar } from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "The Commune",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased`}>
+        <Navbar />
         <main>{children}</main>
         <footer>
           <hr />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/posts">News</a>
+    </nav>
+  );
+};
+
+export default Navbar;
