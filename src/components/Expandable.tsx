46e6647419836bf65aadcc8f03b605051fb3c722
f-sage <junia.en@gmail.com>
2026-03-19 21:09:31 +0200

    style the expandable

--- src/components/Expandable.tsx
+++ src/components/Expandable.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import "./Expandable.css";
+import "material-icons/iconfont/material-icons.css";
 
 interface ExpandableProps {
   label: string;
@@ -11,12 +12,18 @@ export const Expandable: React.FC<ExpandableProps> = ({ label, children }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const contentClassName = isExpanded ? "content expanded" : "content";
+  const buttonIcon = isExpanded ? "keyboard_double_arrow_left" : "add";
+  const buttonAriaLabel = isExpanded ? "collapse" : "expand";
   return (
     <div className="expandable">
       <div>
         <span>{label}</span>
-        <button type="button" onClick={() => setIsExpanded(!isExpanded)}>
-          expand
+        <button
+          aria-label={buttonAriaLabel}
+          type="button"
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          <span className="material-icons">{buttonIcon}</span>
         </button>
       </div>
 
