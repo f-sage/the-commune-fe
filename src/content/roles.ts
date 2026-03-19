43731406ae3a2ba577c824813639c90ba1721879
f-sage <junia.en@gmail.com>
2026-03-19 19:04:35 +0200

    Add roles info

---
+++ src/content/roles.ts
@@ -0,0 +1,92 @@
+interface Role {
+  name: string;
+  description: string;
+}
+
+export const roles: Role[] = [
+  {
+    name: "Archivist",
+    description:
+      "Archivists are looter specialists, near guaranteed chance to pick up vessels and the ability to collect clutter and instantly repair it with a chisel in addition to several aged recipes.",
+  },
+  {
+    name: "Blackguard",
+    description:
+      "Blackguards compete for a strong martial status, being able to make their armour inherently without finding it and having several Copper age recipes at a discount on top of some metallurgy bonuses.",
+  },
+  {
+    name: "Brickmaker",
+    description:
+      "Brickmakers have the ability to make bricks and shingles via molds in their grid allowing easy mass production without clayforming on top of cheap recipes for mortar.",
+  },
+  {
+    name: "Butcher",
+    description:
+      "Butchers fill the role of a cook having double the output from carcasses but take longer to do so in their experise. Stone Age access to Querns and the ability to make Cured Meats and Vintage Beef with easy access to Salt.",
+  },
+  {
+    name: "Clockmaker",
+    description:
+      "Clockmakers double down on their vanilla bonuses being dedicated Anti-Drifter specialists and having an expertise towards the manufacturing of automation components while salvaging lots of materials from nests and spikes.",
+  },
+  {
+    name: "Commoner",
+    description:
+      "Commoners are mostly untouched, having a 50% reduction to hunger rates.",
+  },
+  {
+    name: "Farmhand",
+    description:
+      "Farmhands have substantially increased crop returns from Wild Crops and can easily secure seeds on top of cheaper soil and fertilizer recipes.",
+  },
+  {
+    name: "Florist",
+    description:
+      "Florists have the ability to receive papyrus and horsetail from cutting grass with very efficient healing recipes while being able to mass produce beenades.",
+  },
+  {
+    name: "Forester",
+    description:
+      "Foresters double down as carpenters and maintainers of trees and their respective components whether it be from better returns on boards or sticks to charcoal production.",
+  },
+  {
+    name: "Hunter",
+    description:
+      "Hunters are substantially more efficient at producing hides while having generous bonuses to fauna and flora harvesting.",
+  },
+  {
+    name: "Malefactor",
+    description:
+      "Malefactors return with a vengeance with double gear and vessel loot while being able to produce torch holders in stone age.",
+  },
+  {
+    name: "Messenger",
+    description:
+      "Messengers keep everyone moving forward with cheaper path and backpack recipes while being swift of foot.",
+  },
+  {
+    name: "Rust Mage",
+    description:
+      "Rusty Mages are dedicated spellweavers but pay for their hubris in being less efficient in most occupations not related to magic.",
+  },
+  {
+    name: "Quarrier",
+    description:
+      "Quarriers help round out Archivists and Brickmakers with stone specific recipes and substantially more efficient at collecting stone and rock for their trouble.",
+  },
+  {
+    name: "Spelunker",
+    description:
+      "Spelunkers are the best miners while having increased returns in by products or transforming stones into gravel and sand.",
+  },
+  {
+    name: "Tailor",
+    description:
+      "Tailors double down on flax production, can produce fibers from grass or thatch while collecting both easier.",
+  },
+  {
+    name: "Vintner",
+    description:
+      "Vintners keep everyone drunk and happy with Stone Age recipes for fruitpress, buckets and barrels and cheaper distillery components while collecting twice as much brewing resources.",
+  },
+];
