c5833c93f654a082be4e960123e1bf1a99f2f092
f-sage <junia.en@gmail.com>
2026-03-19 21:39:40 +0200

    Add the Posts page

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,18 @@
+import type { WP_REST_API_Post as WordpressPost } from "wp-types";
+import PostPreview from "@/components/PostPreview";
+
+export default async function PostsPage() {
+  const baseWordpressUrl = process.env.BASE_WP_URL;
+  const res = await fetch(`${baseWordpressUrl}/wp-json/wp/v2/posts`);
+  const posts = await res.json();
+
+  return (
+    <>
+      <h1>Posts</h1>
+
+      {posts?.map((item: WordpressPost) => (
+        <PostPreview key={item.id} post={item} />
+      ))}
+    </>
+  );
+}
