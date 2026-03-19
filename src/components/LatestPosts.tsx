f9b469304180ed6bd2c5d5d83f02ba45065ecb14
f-sage <junia.en@gmail.com>
2026-03-19 21:45:24 +0200

    Only show 2 latest posts on the main page

--- src/components/LatestPosts.tsx
+++ src/components/LatestPosts.tsx
@@ -3,8 +3,11 @@ import "./LatestPosts.css";
 import type { WP_REST_API_Post as WordpressPost } from "wp-types";
 
 export const LatestPosts = async () => {
+  const postCount = 2;
   const baseWordpressUrl = process.env.BASE_WP_URL;
-  const res = await fetch(`${baseWordpressUrl}/wp-json/wp/v2/posts`);
+  const res = await fetch(
+    `${baseWordpressUrl}/wp-json/wp/v2/posts?per_page=${postCount}`,
+  );
   const posts = await res.json();
   return (
     <div id="posts">
