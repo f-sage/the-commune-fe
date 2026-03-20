fe3ebcfd4a8c0fec9736abeb3db578ade5a0df3b
f-sage <junia.en@gmail.com>
2026-03-20 17:33:46 +0200

    Add radix UI theme to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./layout.css";
+import { Theme } from "@radix-ui/themes";
 import { Navbar } from "@/components/Navbar";
 
 export const metadata: Metadata = {
@@ -16,17 +17,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased`}>
-        <Navbar />
-        <main>{children}</main>
-        <footer>
-          <hr />
-          <ul>
-            <li>
-              Maintained by <em>TheButterMan</em>
-            </li>
-            <li>2026</li>
-          </ul>
-        </footer>
+        <Theme>
+          <Navbar />
+          <main>{children}</main>
+          <footer>
+            <hr />
+            <ul>
+              <li>
+                Maintained by <em>TheButterMan</em>
+              </li>
+              <li>2026</li>
+            </ul>
+          </footer>
+        </Theme>
       </body>
     </html>
   );
