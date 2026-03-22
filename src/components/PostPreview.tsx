c08c714b344a80b8d03fdb684aedd365b90b9f23
f-sage <junia.en@gmail.com>
2026-03-22 18:52:07 +0200

    Add space and borders between posts

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,10 +1,11 @@
 import type { WP_REST_API_Post as WordpressPost } from "wp-types";
+import "./PostPreview.css";
 
 export const PostPreview = ({ post }: { post: WordpressPost }) => {
   const content = { __html: post.excerpt.rendered };
   const createdAt = new Date(post.date).toDateString();
   return (
-    <article>
+    <article className="post">
       <h3>
         <a href={`/post/${post.id}`}>{post.title.rendered}</a>
       </h3>
