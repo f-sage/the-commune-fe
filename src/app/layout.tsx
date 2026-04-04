10050d81d0fc48157897e2b602a7c1d19fa6c74d
f-sage <junia.en@gmail.com>
2026-04-04 18:05:40 +0300

    remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./layout.css";
 import { Footer } from "@/components/Footer";
-import { Navbar } from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "The Commune",
