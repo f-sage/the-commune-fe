efc7254a19251390c2696a7280c0a9b0cd901dc2
f-sage <junia.en@gmail.com>
2026-04-04 18:04:22 +0300

    Style the hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { LatestPosts } from "@/components/LatestPosts";
 import "./homepage.css";
 import { discordInviteUrl, donateUrl, liveMapUrl } from "@/content/links";
@@ -5,24 +6,31 @@ import { discordInviteUrl, donateUrl, liveMapUrl } from "@/content/links";
 export default async function Home() {
   return (
     <>
-      <h1>
-        <div className="subtitle">Welcome to</div>
-        <div className="loud">The Commune</div>
-      </h1>
+      <header id="hero">
+        <h1>
+          <div className="subtitle">Welcome to</div>
+          <div className="loud">The Commune</div>
+        </h1>
 
-      <div id="description">
-        <p>
-          The Commune is a cooperative Vintage Story community where creativity,
-          collaboration, and long-term progression are at the heart of the
-          experience. This is a place for players who enjoy building together,
-          planning infrastructure, and participating in community-driven
-          projects.
-        </p>
+        <div id="description">
+          <p>
+            The Commune is a cooperative Vintage Story community where
+            creativity, collaboration, and long-term progression are at the
+            heart of the experience. This is a place for players who enjoy
+            building together, planning infrastructure, and participating in
+            community-driven projects.
+          </p>
 
-        <a id="details-link" href="/about">
-          Server details
-        </a>
-      </div>
+          <a id="details-link" href="/about">
+            Server details
+          </a>
+        </div>
+        <Image
+          alt="You too can live among beauty"
+          src="/banner.png"
+          layout="fill"
+        />
+      </header>
 
       <ul id="links">
         <li>
