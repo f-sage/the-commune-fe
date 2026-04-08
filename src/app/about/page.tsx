84999ed54378a76bcf13a641c9748506fe3d3d56
f-sage <junia.en@gmail.com>
2026-04-08 22:13:35 +0300

    Remove old content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,17 +7,6 @@ export default async function AboutServer() {
     <>
       <h1>Server details</h1>
       <h2>The class system</h2>
-
-      <p>The server uses a custom class system with class locked recipes.</p>
-      <ul id="role-list">
-        {roles.map((item) => (
-          <li key={item.name}>
-            <Expandable label={item.name}>
-              <p>{item.description}</p>
-            </Expandable>
-          </li>
-        ))}
-      </ul>
     </>
   );
 }
