dc1e11624464a67bcbb483cdb7672e2212d08593
f-sage <junia.en@gmail.com>
2026-04-17 13:13:50 +0300

    Process discord messages to get the mod list in the correct format

--- src/helpers/discordPostHelpers.ts
+++ src/helpers/discordPostHelpers.ts
@@ -31,20 +31,20 @@ export const stripMarkup = (text: string) => {
   return text;
 };
 
+export const markupMatchers = {
+  bold: /\*\*(.*?)\*\*/g,
+  italic: /\*(.*?)\*/g,
+  underlined: /__(.*?)__/g,
+  strikethrough: /~~(.*?)~~/g,
+  link: /\[(.*?)\]\((.*?)\)/g,
+  spoiler: /\|\|(.*?)\|\|/g,
+};
+
 // Source - https://stackoverflow.com/a/16947614
 // Posted by n1xx1, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-04-05, License - CC BY-SA 3.0
 
 export const textToHtmlMarkup = (text: string) => {
-  const markupMatchers = {
-    bold: /\*\*(.*?)\*\*/g,
-    italic: /\*(.*?)\*/g,
-    underlined: /__(.*?)__/g,
-    strikethrough: /~~(.*?)~~/g,
-    link: /\[(.*?)\]\((.*?)\)/g,
-    spoiler: /\|\|(.*?)\|\|/g,
-  };
-
   text = text.replace(markupMatchers.bold, "<strong>$1</strong>");
   text = text.replace(markupMatchers.underlined, "<u>$1</u>");
   text = text.replace(markupMatchers.italic, "<em>$1</em>");
